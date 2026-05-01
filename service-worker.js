f520c0477b3c8cb25b92f11e913bd2902fffe485
pandujatip <pamungkas29051993@gmail.com>
2026-05-01 16:41:57 +0700

    Use PNG app icon for visible logo

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "plirm34-pwa-v20260501-06";
+const CACHE_NAME = "plirm34-pwa-v20260501-07";
 const APP_SHELL = [
   "/",
   "/index.html",
-  "/styles.css?v=20260501-06",
+  "/styles.css?v=20260501-07",
   "/app.auth.js?v=20260426-15",
   "/app.carbon-brush.js?v=20260501-05",
   "/app.service.js?v=20260426-17",
@@ -13,8 +13,7 @@ const APP_SHELL = [
   "/manifest.webmanifest",
   "/pwa-icons/icon-192.png",
   "/pwa-icons/icon-512.png",
-  "/pwa-icons/icon-maskable-512.png",
-  "/pwa-icons/plirm34tuban.jpg"
+  "/pwa-icons/icon-maskable-512.png"
 ];
 
 self.addEventListener("install", (event) => {
